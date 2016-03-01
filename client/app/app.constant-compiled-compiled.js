9bb06f0baf4bec50c244c121aa86080dbb94eaf5
MOHAMMED TSALIS <m.salis@go-jek.com>
2016-03-01 23:32:51 +0700

    running gulp serve:dist

---
+++ client/app/app.constant-compiled-compiled.js
@@ -0,0 +1,9 @@
+(function (angular, undefined) {
+	angular.module("memoryApp.constants", []).constant("appConfig", {
+		"userRoles": ["guest", "user", "admin"]
+	});
+})(angular);
+
+//# sourceMappingURL=app.constant-compiled.js.map
+
+//# sourceMappingURL=app.constant-compiled-compiled.js.map
