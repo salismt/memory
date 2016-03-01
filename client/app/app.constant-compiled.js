43e2281d733656ae4bb08b782d902eea73858e1b
MOHAMMED TSALIS <m.salis@go-jek.com>
2016-03-01 17:18:42 +0700

    change Mongodb host for heroku

---
+++ client/app/app.constant-compiled.js
@@ -0,0 +1,7 @@
+(function (angular, undefined) {
+	angular.module("memoryApp.constants", []).constant("appConfig", {
+		"userRoles": ["guest", "user", "admin"]
+	});
+})(angular);
+
+//# sourceMappingURL=app.constant-compiled.js.map
